be9de753671c2d0ab5f46a795fa65d6c35b26791
kenlok <ken.lok@eezee.com.sg>
2022-03-12 18:28:36 +0800

    (fix):  add responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 
 import PageContainer from 'components/common/page-container'
 
-import Section1 from 'components/home/section1'
+import Home from 'components/home'
 
 export default function HomePage() {
     return (
         <PageContainer>
-            <Section1 />
+            <Home />
         </PageContainer>
     )
 }
