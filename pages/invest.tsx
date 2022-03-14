ee571b4c99241a56d2525ed107d100b0eafdb4a3
kenlok <ken.lok@eezee.com.sg>
2022-03-14 23:18:20 +0800

    add invest page

---
+++ pages/invest.tsx
@@ -0,0 +1,12 @@
+
+import PageContainer from 'components/common/page-container'
+
+import Invest from 'components/invest/invest'
+
+export default function InvestPage() {
+    return (
+        <PageContainer>
+            <Invest />
+        </PageContainer>
+    )
+}
