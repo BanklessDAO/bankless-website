ba8f02390664151fd6fea1a2fde192108af59a02
kenlok <ken.lok@eezee.com.sg>
2022-04-04 22:25:22 +0800

    add gif to homepage

--- components/home/section1.tsx
+++ components/home/section1.tsx
@@ -36,13 +36,11 @@ class Section1 extends React.Component {
           <Link fontSize='2xl' href='https://twitter.com/banklessDAO'>Twitter</Link>
           <Link fontSize='2xl' href='/new-members'>New Members Start</Link>
         </HStack>  
-        
-        <Image
-          src='https://s3-alpha-sig.figma.com/img/0d4d/fb58/54a73a14f7a41a09c96399acb634a29d?Expires=1646006400&Signature=IH8AXlx7ICC~PgYQIzFVNAUpOIeST-pImvgQEKnewMIn-LF1s-E0Y6rCAFltjbjwb1u4JBgVh8lvMzIxcZ0nEvKHM58qB4JxXR~naGC8AntEZoOqLS-1bIfqrsFEvkMs-uRzCeHoJKH5YBtTdRD6-rRF7ZlmUqkB0az11R3jUG4FnlKhIEvyoZAyGbIyF-xxZXIJWDBgfx5OAdo44Iv1IOuKq7XG80Bh-uSBPCgo-UjJ-pbJY942LLAxmmo8uSWmTHPMwnRl8326sANGRDL~YjN-~fvOAQTEf3bGlfiSEp38xCGcQUmT8QHOS79N9dLiLhpabmJQYLzKgQUnBiyrlQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
-          height='600'
-          width='800'
-          alt='bankless-gif'
-        />
+
+        <video width="800" height="600" autoPlay loop>
+          <source src="/videos/DAOhandLogo.mp4" type="video/mp4" />
+        </video>
+
       </Flex>
     )
   }
