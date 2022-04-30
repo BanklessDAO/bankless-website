c61e1ceff83f5b2dc8eba844ffeb1e7d90384027
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-05-01 00:33:12 +0100

    FEAT (work-with-us): updates the section images
    Adds new assets

--- components/work-with-us/Market/TabContent.tsx
+++ components/work-with-us/Market/TabContent.tsx
@@ -30,10 +30,8 @@ export default function MarketTabContentComponent() {
         position={'relative'}
         w={'full'}>
         <ConsultImage
-          alt={'Hero Image'}
-          src={
-            'https://s3-alpha-sig.figma.com/img/7d79/9fd3/9acaaa1c332c27aafb28b7b67c29b95d?Expires=1649030400&Signature=JVP0rKoQyQJN2CSb9YpWhRpqI-Pljbp2aiTIGkw85cbnSumkGmgx0rE90MLwqIqSDsc6n-~Vtpm4LYLtNX115jLY1ju1lJ82QIxRC6JB-Pm0HBysjXk3JIZWS0IwoLGGm1zWqzSiVR~~VBRHDwA4Nie8pUhqXP6UA2SKeHrtGJGESBdLVrIXgo3OEriVNuIkf--hQgzn0iKlJP2m0lUwoeDWx2-99q59di8Lh6OkAXZaU6W9SJrlg4GgbLLhwTnXuzPuTfhusysUyvz83BwrpK7RmdR0TF1tKVC870483Ib9U0x5Sr6fYnNF64gLwBy~rRnbHvN5j~pS2WuVB09BRg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
-          }
+          alt={'Photo by Jess Bailey on Unsplash'}
+          src={'/images/work-with-us/work-with-us-5.jpg'}
         />
       </Flex>
     </Stack>
