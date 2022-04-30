561497190ba21ab5d2d034929bf9b2ea89d9f9f1
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-04-30 23:50:31 +0100

    FEAT (consult): implements the new image style

--- components/work-with-us/Consult/TabContent.tsx
+++ components/work-with-us/Consult/TabContent.tsx
@@ -9,6 +9,7 @@ import {
   Input,
   useColorModeValue,
 } from '@chakra-ui/react'
+import { ConsultImage } from '../ConsultImage'
 
 export default function ConsultTabContentComponent() {
   return (
@@ -16,48 +17,29 @@ export default function ConsultTabContentComponent() {
       align={'center'}
       spacing={{ base: 8, md: 10 }}
       pt={{ base: 10, md: 14 }}
-      direction={{ base: 'column', md: 'row' }}
-    >
+      direction={{ base: 'column', md: 'row' }}>
       <Flex
         flex={1}
         justify={'center'}
         align={'center'}
         position={'relative'}
-        w={'full'}
-      >
-        <Box
-          position={'relative'}
-          height={'300px'}
-          rounded={'2xl'}
-          boxShadow={'2xl'}
-          width={'full'}
-          overflow={'hidden'}
-        >
-          <Image
-            alt={'Hero Image'}
-            fit={'cover'}
-            align={'center'}
-            w={'100%'}
-            h={'100%'}
-            src={
-              'https://s3-alpha-sig.figma.com/img/1b8e/c2e1/0b5980279143712e32d4c395d1dd8f4b?Expires=1649030400&Signature=VprOU20kNkOAzM~sM6CFdIck8LZvTWqZbhgLT8ZoSFFkNeZk6kNZ1eOJAg8vKf5IW~pdPvWIP6iTENiEjWjVXk-PAzu3l3WW1AAf1DKl44DtzoPKUWvTRHGu5mmeY3GzFjlO5~E41sSr3txCAWZjm1unrzCHgdhDMylT-lCd~NQKwtYNAB62Tvs6oV84jKTUCUxSBQkQJr7tkKXqHnMzMbqe0C7mySWVzc1f-m0S9plNqa~MxxUAZ7UN3JDPuNfVNk0TknVmfi87ZMUjKBc8ricvbgoSSZtYUxrX1kyXBgRYUVeDYUKC6vRgC1FFBZ6CBuE42cDCD1-0mEJ9C3NaWQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
-            }
-          />
-        </Box>
+        w={'full'}>
+        <ConsultImage src={'/images/work-with-us/work-with-us-2.jpg'} alt={'Hero Image'}/>
       </Flex>
 
       <Stack flex={1} spacing={{ base: 5, md: 10 }}>
-        <Text color={'gray.100'} fontSize={{ sm: 'sm', md: 'md' }}>
+        <Text color={'gray.100'} fontSize={{ sm: 'sm', md: 'xl' }} textAlign="left">
           Our Web3-native consulting service draws on the expertise of bDAO
-          members to help DAOs launch and scale their presence. Bankless
-          Consultancy also helps traditional organizations take the first step
+          members to help DAOs launch and scale their presence. 
+        </Text>
+        <Text color={'gray.100'} fontSize={{ sm: 'sm', md: 'xl' }} textAlign="left">
+          Bankless Consultancy also helps traditional organizations take the first step
           into Web3.
         </Text>
         <Stack
           direction={{ base: 'row', md: 'row' }}
           as={'form'}
-          spacing={'12px'}
-        >
+          spacing={'12px'}>
           <FormControl>
             <Input
               variant={'solid'}
