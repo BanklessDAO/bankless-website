27797615ad7d58a4239bdf13cfcf9bcecb00372e
kenlok <ken.lok@eezee.com.sg>
2022-05-01 10:56:20 +0800

    FEAT (home): make bank token and eth blockchain clickable

--- components/home/section3.tsx
+++ components/home/section3.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
-
-import { Flex, Text, Box, Button, Image } from '@chakra-ui/react'
-import Underline from 'components/common/underline'
+import React from 'react';
+import { Flex, Text, Box, Button, Image, Link } from '@chakra-ui/react';
+import Underline from 'components/common/underline';
 
 class Section3 extends React.Component {
   render() {
@@ -25,46 +24,51 @@ class Section3 extends React.Component {
           justifyContent='space-evenly'
           width='100%'
         >
-          <Flex
-            height='fit-content'
-            flexDirection='column'
-            alignItems='center'
-            margin={6}
-          >
-            <Box>
-              <Image
-                src='/icons/bankless-coin.png'
-                alt='bankless-coin'
-                height={200}
-                width={200}
-                objectFit='contain'
-              />
-            </Box>
-            <Box>
-              <Text fontSize='4xl' fontWeight={700}>
-                BANK Token
+          <Link href={'https://etherscan.io/token/0x2d94aa3e47d9d5024503ca8491fce9a2fb4da198'}>
+            <Flex
+              height='fit-content'
+              flexDirection='column'
+              alignItems='center'
+              margin={6}
+            >
+
+              <Box>
+                <Image
+                  src='/icons/bankless-coin.png'
+                  alt='bankless-coin'
+                  height={200}
+                  width={200}
+                  objectFit='contain'
+                />
+              </Box>
+              <Box>
+                <Text fontSize='4xl' fontWeight={700}>
+                  BANK Token
+                </Text>
+              </Box>
+            </Flex>
+          </Link>
+          <Link href={'https://ethereum.org/en/'}>
+            <Flex
+              height='fit-content'
+              flexDirection='column'
+              alignItems='center'
+              margin={6}
+            >
+              <Box>
+                <Image
+                  src='/icons/ethereum-icon.png'
+                  alt='ethereum-icon'
+                  height={200}
+                  width={200}
+                  objectFit='contain'
+                />
+              </Box>
+              <Text fontSize='4xl' fontWeight={700} textAlign='center'>
+                Ethereum Blockchain
               </Text>
-            </Box>
-          </Flex>
-          <Flex
-            height='fit-content'
-            flexDirection='column'
-            alignItems='center'
-            margin={6}
-          >
-            <Box>
-              <Image
-                src='/icons/ethereum-icon.png'
-                alt='ethereum-icon'
-                height={200}
-                width={200}
-                objectFit='contain'
-              />
-            </Box>
-            <Text fontSize='4xl' fontWeight={700} textAlign='center'>
-              Ethereum Blockchain
-            </Text>
-          </Flex>
+            </Flex>
+          </Link>
         </Flex>
 
         <Flex
