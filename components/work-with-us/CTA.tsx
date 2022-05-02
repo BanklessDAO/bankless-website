b61adf7c5b6cccc9ee97f5c09989afa056af92fe
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-05-02 19:33:51 +0100

    REFACTOR (CTA) : cleaned up the CTA component

--- components/work-with-us/CTA.tsx
+++ components/work-with-us/CTA.tsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import {
-  //   Stack,
-  //   FormControl,
   Button,
-  //   Input,
-  //   useColorModeValue,
 } from '@chakra-ui/react'
 import Link from 'next/link'
-import { string } from 'yup'
 
 type CTAProps = {
   text: string
@@ -15,34 +10,9 @@ type CTAProps = {
 }
 
 export const CTA = ({ text, url }: CTAProps) => (
-  <>
-    {/* <Stack direction={{ base: 'row', md: 'row' }} as={'form'} spacing={'12px'}>
-      <FormControl>
-      <Input
-        variant={'solid'}
-        borderWidth={1}
-        color={'gray.800'}
-        _placeholder={{
-          color: 'gray.400',
-        }}
-        borderColor={useColorModeValue('gray.300', 'gray.700')}
-        id={'contact'}
-        type={'text'}
-        required
-        placeholder={'Get in touch by filling out this contact form'}
-        aria-label={'contact form'}
-      />
-    </FormControl>
-    <FormControl w={{ base: '30%', md: '40%' }}>
-      <Button colorScheme='red' w='100%' type='submit'>
-        Fill the Form
-      </Button>
-    </FormControl>
-    </Stack> */}
-    <Link href={url}>
-      <Button colorScheme='red' w='fit-content'>
-        <a>Visit {text}</a>
-      </Button>
-    </Link>
-  </>
+  <Link href={url}>
+    <Button colorScheme='red' w='fit-content'>
+      <a>Visit {text}</a>
+    </Button>
+  </Link>
 )
