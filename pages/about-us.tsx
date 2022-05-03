0299b43bd2630486b537049a894eab04f0a4b127
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-05-03 18:54:48 +0100

    FIX (PageContent + page templates) : included the responsive styles in the PageContent component
    Implemented the PageContent within _app
    
    Removed all previous instances of the PageComponent on each individual page

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -1,5 +1,3 @@
-import PageContainer from 'components/common/page-container'
-import { Container } from '@chakra-ui/react'
 import IntroductionAboutUsComponent from 'components/about-us/introduction'
 import OurMissionAboutUsComponent from 'components/about-us/our-mission'
 import OurVisionAboutUsComponent from 'components/about-us/our-vision'
@@ -8,14 +6,12 @@ import NewsLetterAboutUsComponent from 'components/common/newsletter-section'
 
 export default function AboutUsPage() {
   return (
-    <PageContainer>
-      <Container maxW={{ base: 'container.md', md: 'container.xl' }}>
+      <>
         <IntroductionAboutUsComponent />
         <OurMissionAboutUsComponent />
         <OurVisionAboutUsComponent />
         <JoinUsAboutUsComponent />
         <NewsLetterAboutUsComponent />
-      </Container>
-    </PageContainer>
+      </>
   )
 }
--- pages/invest.tsx
+++ pages/invest.tsx
@@ -1,10 +1,5 @@
-import PageContainer from 'components/common/page-container'
 import Invest from 'components/invest/Invest'
 
 export default function InvestPage() {
-  return (
-    <PageContainer>
-      <Invest />
-    </PageContainer>
-  )
+  return <Invest />
 }
