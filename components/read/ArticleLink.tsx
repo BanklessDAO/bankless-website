26c58ded9152f5c71482a09a1bc32a15cd375f7d
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-05-07 04:31:18 +0100

    FEAT (/read) : updates the articles into reusable comp

---
+++ components/read/ArticleLink.tsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import { Button, HStack, Text, Stack, Flex, Image } from '@chakra-ui/react'
+import Link from 'next/link'
+
+type ArticleLinkProps = {
+  type: string
+  url: string
+  desc: string
+}
+
+type ArticleIconProps = {
+  type: string
+}
+
+const ArticleIcon = ({ type }: ArticleIconProps) => (
+  <>
+    {type === 'medium' && (
+      <Flex
+        justify='center'
+        alignItems='center'
+        background='#222222'
+        borderRadius='8px'
+        padding='10px'>
+        <Image src='/icons/medium.png' alt='medium' height={30} width={45} />
+      </Flex>
+    )}
+    {type === 'mirror' && (
+      <Flex
+        justify='center'
+        alignItems='center'
+        background='#222222'
+        borderRadius='8px'
+        padding='10px'>
+        <Image src='/icons/mirror.png' alt='mirror' height={30} width={25} />
+      </Flex>
+    )}
+    {!type && <></>}
+  </>
+)
+
+function setArticleType(type: string): string | undefined {
+  switch (type) {
+    case 'medium':
+      return 'Medium Article'
+    case 'mirror':
+      return 'Mirror Article'
+    default:
+      break
+  }
+}
+
+export const ArticleLink = ({ type, url, desc }: ArticleLinkProps) => (
+  <Link href={url}>
+    <Button
+      as='a'
+      d='flex'
+      flexDirection='column'
+      alignItems='start'
+      target='_blank'
+      w='100%'
+      p={0}
+      height='100%'
+      bg='transparent'
+      zIndex={1}
+      _hover={{ backgroundColor: 'yellow', cursor: 'pointer' }}>
+      <Flex
+        justify='center'
+        alignItems='center'
+        background='#222222'
+        borderRadius='8px'
+        padding='10px'>
+        <ArticleIcon type={type} />
+      </Flex>
+      <HStack justifyContent='center' alignItems='baseline'>
+        <Text fontSize={{ base: '3xl', md: '4xl' }} fontWeight={700}>
+          {setArticleType(type)}
+        </Text>
+        <Image src='/icons/arrow.png' alt='arrow' height={15} width={15} />
+      </HStack>
+      <HStack w='30px'>
+        <Text fontSize='xl'>{desc}</Text>
+      </HStack>
+    </Button>
+  </Link>
+)
