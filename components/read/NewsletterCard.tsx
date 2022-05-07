6af6deec79f72428db4f683922a45395505fc92c
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-05-07 19:36:56 +0100

    STYLES (/read) : fixes newsletter cards
    Also adds some animation to the bankless chips

--- components/read/NewsletterCard.tsx
+++ components/read/NewsletterCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import NextLink from 'next/link'
-import { Text, Box, GridItem, Link, transition } from '@chakra-ui/react'
+import { Text, Box, GridItem, Link, keyframes, VStack } from '@chakra-ui/react'
 import { ChakraNextImage } from 'components/common/ChakraNextImage'
 type NewsletterCardProps = {
   title: string
@@ -17,6 +17,12 @@ type NewsletterCardImage = {
   alt: string
 }
 
+const keyframeSideToSide = keyframes`
+  0% {transform: translateX(0rem)}
+  50% {transform: translateX(2rem)}
+  100% {transform: translateX(0rem)}
+`;
+
 export const NewsletterCard = ({
   title,
   text,
@@ -25,26 +31,26 @@ export const NewsletterCard = ({
   rowSpan,
   image,
 }: NewsletterCardProps) => {
+  const animationSideToSide = `${keyframeSideToSide} infinite 4s ease-in-out forwards`;
   return (
-    <NextLink href={href}>
-      <GridItem
-        as='li'
-        alignItems='start'
-        bgColor={'#101010'}
-        padding={{ base: 4, md: 8 }}
-        borderRadius={6} 
-        m={0}
-        rowSpan={{ base: 1, xl: rowSpan || 1 }}
-        colSpan={{ base: 1, xl: colSpan || 1 }}
-        listStyleType={'none'}
-        pos='relative'
-        border='1px solid rgba(255, 0, 0, 0)'
-        _hover={{
-          textDecoration: 'none',
-          backgroundColor: 'rgba(16, 16, 16, 0.6)',
-          transition: 'background-color 125ms ease-in-out',
-          border: '1px solid rgba(255, 0, 0, 0.5)',
-        }}>
+    <GridItem
+      as='li'
+      alignItems='start'
+      bgColor={'#101010'}
+      borderRadius={6}
+      m={0}
+      rowSpan={{ base: 1, xl: rowSpan || 1 }}
+      colSpan={{ base: 1, xl: colSpan || 1 }}
+      listStyleType={'none'}
+      pos='relative'
+      border='1px solid rgba(255, 0, 0, 0)'
+      _hover={{
+        textDecoration: 'none',
+        backgroundColor: 'rgba(16, 16, 16, 0.6)',
+        transition: 'background-color 125ms ease-in-out',
+        border: '1px solid rgba(255, 0, 0, 0.5)',
+      }}>
+      <NextLink href={href} passHref={true}>
         <Link
           alt={`Visit ${title}`}
           textDecoration='none'
@@ -52,47 +58,58 @@ export const NewsletterCard = ({
             textDecoration: 'none',
             backgroundColor: 'rgba(16, 16, 16, 0.7)',
           }}>
-          <Text fontSize={'4xl'} fontWeight={700} fontFamily='Clear Sans'>
-            {title}
-          </Text>
-          <Text
-            fontSize='xl'
-            textOverflow='wrap'
-            fontFamily='Clear Sans'
-            color='grey'
-            lineHeight='1.2'
-            m={0}
-            maxW='80%'>
-            {text}
-          </Text>
-          <Box display='flex' alignItems='center' marginRight='2' marginTop={6}>
-            <Text fontSize='xl' marginRight={2} fontFamily='Clear Sans'>
-              View
+          <VStack p={{ base: 4, md: 8 }} alignItems='start'>
+            <Text fontSize={'4xl'} fontWeight={700} fontFamily='Clear Sans'>
+              {title}
             </Text>
-            <Box>
-              <Image
-                src='/icons/arrow.png'
-                alt='arrow'
-                height={10}
-                width={10}
-              />
+            <Text
+              fontSize='xl'
+              textOverflow='wrap'
+              fontFamily='Clear Sans'
+              color='grey'
+              lineHeight='1.2'
+              m={0}
+              maxW='80%'>
+              {text}
+            </Text>
+            <Box
+              display='flex'
+              alignItems='center'
+              marginRight='2'
+              marginTop={6}>
+              <Text fontSize='xl' marginRight={2} fontFamily='Clear Sans'>
+                View
+              </Text>
+              <Box>
+                <Image
+                  src='/icons/arrow.png'
+                  alt='arrow'
+                  height={10}
+                  width={10}
+                />
+              </Box>
             </Box>
-          </Box>
-          {image && (
-            <ChakraNextImage
-              d={{ base: 'none', xl: 'block' }}
-              src={image.src}
-              alt={image.alt}
-              pos='absolute'
-              width='30rem'
-              height='28rem'
-              zIndex={2}
-              bottom={{ base: -10, xl: -20 }}
-              right={{ base: -5, xl: 10 }}
-            />
-          )}
+            {image && (
+              <ChakraNextImage
+                d={{ base: 'none', xl: 'block' }}
+                src={image.src}
+                alt={image.alt}
+                pos='absolute'
+                width='30rem'
+                height='28rem'
+                zIndex={2}
+                bottom={{ base: -10, xl: -20 }}
+                right={{ base: -5, xl: 10 }}
+                _hover={{
+                  // transform: 'translateX(6rem)',
+                  // transition: 'transform 250ms ease-in-out',
+                  animation: animationSideToSide
+                }}
+              />
+            )}
+          </VStack>
         </Link>
-      </GridItem>
-    </NextLink>
+      </NextLink>
+    </GridItem>
   )
 }
