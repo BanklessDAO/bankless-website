0443f7d886d056a46f7d2a0aa700b5a08ce9c813
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-05-14 00:46:25 +0100

    FIX (/about-us) : remove the ThreeCircles

--- components/_common/newsletter-section.tsx
+++ components/_common/newsletter-section.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 
-import {
-  Flex,
-} from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
 import ThreeRings from './three-rings'
 import SubscribeWidget from './subscribe-widget'
 
 class NewsletterSection extends React.Component {
   render() {
     return (
-      <Flex flexDir='column' justifyContent='center' alignItems='center' rowGap={{ base: 8, md: 16 }}>
-        <ThreeRings />
+      <Flex
+        flexDir='column'
+        justifyContent='center'
+        alignItems='center'
+        rowGap={{ base: 8, md: 16 }}
+        mt={16}>
+        {/* <ThreeRings /> */}
         <SubscribeWidget />
-
       </Flex>
     )
   }
