ecccacb7cf30510a2c94d0dcdba0cc2e5e70da7c
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-05-14 01:12:31 +0100

    FIX (/about-us): updates the text for our-mission and our-vission section

--- components/about-us/our-vision.tsx
+++ components/about-us/our-vision.tsx
@@ -88,6 +88,8 @@ export default function OurVisionIntroComponent() {
     false,
     false,
   ])
+  const pxBase = 0
+  const pxMd = 40
 
   return (
     <>
@@ -95,9 +97,8 @@ export default function OurVisionIntroComponent() {
         as={Box}
         textAlign={'center'}
         spacing={{ base: 4, md: 7 }}
-        py={{ base: 10, md: 18 }}
-        mt='20'
-      >
+        // py={{ base: 10, md: 18 }}
+        mt='20'>
         <link
           rel='stylesheet'
           type='text/css'
@@ -114,19 +115,18 @@ export default function OurVisionIntroComponent() {
           fontSize={{ base: '3xl', sm: '3xl', md: '4xl' }}
           lineHeight={'110%'}
           color={'white.100'}
-          textTransform={'uppercase'}
-        >
-          Our Vision is to make financial independence, decentralized work, and
-          creative freedom accessible to all
+          textTransform={'uppercase'}>
+          Our Vision
           <Underline />
         </Heading>
-
         <Text
           color={'grey'}
-          px={{ base: 18, md: 32 }}
+          w={{ base: '95%', lg: '100%' }}
+          px={{ base: pxBase, lg: pxMd }}
           fontSize={{ base: 'xl', md: '2xl' }}
-        >
-          We work to build a world where anyone with an internet connection has
+          textAlign={{ base: 'left', lg: 'center' }}>
+          Our Vision is to make financial independence, decentralized work, and
+          creative freedom accessible to all. We work to build a world where anyone with an internet connection has
           access to the tools needed to achieve financial independence. We want
           everyone to be able to open a computer, find a DAO, start working, and
           get paid. We want to enable visionaries to unlock their true creative
@@ -138,8 +138,7 @@ export default function OurVisionIntroComponent() {
           color={'white.100'}
           lineHeight={1.5}
           fontWeight={600}
-          fontSize={{ base: '2xl', md: '3xl' }}
-        >
+          fontSize={{ base: '2xl', md: '3xl' }}>
           OUR VALUES
         </Heading>
         <Box width={'100%'}>
@@ -161,29 +160,25 @@ export default function OurVisionIntroComponent() {
                     const temp = popups
                     temp[index] = !popups[index]
                     setPopups([...temp])
-                  }}
-                >
+                  }}>
                   <Image alt='card' src={card.image} width='94%' mx='3%' />
                   <br />
                   {popups[index] ? (
                     <Box
                       width={'94%'}
                       minHeight='200px'
-                      mt={{ base: '-400px', md: '-160px' }}
-                    >
+                      mt={{ base: '-400px', md: '-160px' }}>
                       <Text
                         color={'white.100'}
                         fontSize={{ base: '50px', md: '20px' }}
                         textTransform={'uppercase'}
-                        whiteSpace='pre'
-                      >
+                        whiteSpace='pre'>
                         {card.title}
                       </Text>
                       <Text
                         color={'white.100'}
                         fontSize={{ base: '20px', md: '12px' }}
-                        px='5'
-                      >
+                        px='5'>
                         {card.text}
                       </Text>
                     </Box>
@@ -192,14 +187,12 @@ export default function OurVisionIntroComponent() {
                       width={'94%'}
                       minHeight='200px'
                       mt={{ base: '-300px', md: '-100px' }}
-                      transitionDuration={'1s'}
-                    >
+                      transitionDuration={'1s'}>
                       <Text
                         color={'white.100'}
                         fontSize={{ base: '50px', md: '20px' }}
                         textTransform={'uppercase'}
-                        whiteSpace='pre'
-                      >
+                        whiteSpace='pre'>
                         {card.title}
                       </Text>
                     </Box>
@@ -221,8 +214,7 @@ export default function OurVisionIntroComponent() {
               background: 'transparent',
               color: 'white.400',
             }}
-            onClick={() => slider?.slickPrev()}
-          >
+            onClick={() => slider?.slickPrev()}>
             <BsArrowLeftCircle size='40px' />
           </IconButton>
           <IconButton
@@ -238,8 +230,7 @@ export default function OurVisionIntroComponent() {
               background: 'transparent',
               color: 'white.400',
             }}
-            onClick={() => slider?.slickNext()}
-          >
+            onClick={() => slider?.slickNext()}>
             <BsArrowRightCircle size='40px' />
           </IconButton>
         </Box>
