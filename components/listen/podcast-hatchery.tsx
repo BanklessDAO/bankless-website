31fbc499bd9500865712e33f1a4002ac6dac5078
gourav <gvirus21yt@gmail.com>
2022-05-24 11:35:00 +0530

    Added responsiveness to top section of Listen page
    - Adjusted all the font sizes and text width
    - Adjusted width and height of image
    - Optimized site for tablet and mobile view
    - Updated UI of button

--- components/listen/podcast-hatchery.tsx
+++ components/listen/podcast-hatchery.tsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
-import {
-  Flex,
-  Box,
-  Text,
-  Button,
-  VStack
-} from '@chakra-ui/react';
+import { Flex, Box, Text, Button, VStack } from '@chakra-ui/react'
+import { HiArrowNarrowRight } from 'react-icons/hi'
 
 class PodcastHatchery extends React.Component {
   render() {
@@ -15,38 +10,81 @@ class PodcastHatchery extends React.Component {
         flexDirection={{ base: 'column-reverse', md: 'row' }}
         justifyContent='center'
         alignItems='center'
-        >
-        <VStack 
+      >
+        <VStack
           maxWidth='600px'
+          mb='4rem'
           justifyContent='start'
           alignItems='start'
-          rowGap={6}>
-          <Text fontWeight={700} fontSize='4xl'>
+          rowGap={6}
+        >
+          <Text
+            fontWeight={700}
+            fontSize={{ base: '1.4rem', sm: '2rem', lg: '3rem' }}
+            // bgColor={{
+            //   base: 'pink',
+            //   sm: 'red',
+            //   md: 'green',
+            //   lg: 'blue',
+            //   xl: 'violet',
+            // }}
+          >
             The Podcast Hatchery
           </Text>
-          <Text>
-            Incubates audio storytelling projects. The team helps new podcasters find their audience,
-            build their unique storytelling identity, and explore Web3 through interviews with
-            thought-leaders in the space.
+          <Text
+            fontSize={{ base: '12px', md: '1rem', lg: '1.7rem' }}
+            width={{ md: '25rem', lg: '32rem' }}
+          >
+            Incubates audio storytelling projects. The team helps new podcasters
+            find their audience, build their unique storytelling identity, and
+            explore Web3 through interviews with thought-leaders in the space.
           </Text>
-          <Button onClick={() => window.open('https://open.spotify.com/show/1jLNOoJv77K733rZzL4c6b')}>
-            Podcast Hatchery
+          <Button
+            onClick={() =>
+              window.open(
+                'https://open.spotify.com/show/1jLNOoJv77K733rZzL4c6b'
+              )
+            }
+            h={{ base: '4rem', md: '3rem', lg: '4rem' }}
+            w={{ base: '16rem', md: '12rem ', lg: '16rem' }}
+            rounded='md'
+            display={'flex'}
+            alignItems='center'
+          >
+            <Text
+              mt={{ base: '-5px', lg: '-8px' }}
+              mr='5px'
+              fontSize={{ base: '1.3rem', md: '0.9rem', lg: '1.3rem' }}
+            >
+              Podcast Hatchery
+            </Text>
+            <HiArrowNarrowRight size={'1.8rem'} />
           </Button>
-          <Text maxWidth='350px' fontSize='lg' color='grey'>
-            Check out these shows produced and
-            hosted by members of BanklessDAO:
+          <Text
+            width={{ base: '13rem', md: '20rem', lg: '350px' }}
+            fontSize={{ base: '12px', md: '1rem', lg: '1.2rem' }}
+            color='grey'
+          >
+            Check out these shows produced and hosted by members of BanklessDAO:
           </Text>
         </VStack>
-        <Box>
-          <Image src="/listen/podcast-hatchery.png"
-            alt="podcast-hatchery"
-            height={450}
-            width={450} />
+        <Box
+          mt={{ base: '-1rem', md: '-5rem', lg: '0' }}
+          ml={{ base: '-3rem', md: '0' }}
+          mb={{ base: '5rem', md: '0' }}
+          height={{ base: '16rem', md: '15rem', lg: '30rem' }}
+          width={{ base: '16rem', md: '15rem', lg: '25rem' }}
+        >
+          <Image
+            src='/listen/podcast-hatchery.png'
+            alt='podcast-hatchery'
+            height={500}
+            width={500}
+          />
         </Box>
-
       </Flex>
     )
   }
 }
 
-export default PodcastHatchery;
+export default PodcastHatchery
