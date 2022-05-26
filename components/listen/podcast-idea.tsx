c17f896bdeb6dc4098ff8339169cad134077d954
gourav <gvirus21yt@gmail.com>
2022-05-26 18:36:45 +0530

    Adjusted Text of podcast-idea for desktop and mobile screen

--- components/listen/podcast-idea.tsx
+++ components/listen/podcast-idea.tsx
@@ -5,17 +5,25 @@ class PodcastIdea extends React.Component {
   render() {
     return (
       <Box>
-        <VStack maxW={700} alignItems='start'>
+        <VStack mt='4rem' maxW={700} alignItems='start'>
           <Text
             fontWeight={700}
-            fontSize={{ base: '4xl', md: '5xl' }}
+            fontSize={{ base: '2xl', md: '6xl' }}
             maxW={450}
           >
             Have your own podcast Idea?
           </Text>
-          <Text fontSize='2xl'>
+
+          <Text
+            width={{ base: '18rem', md: '100%' }}
+            fontSize={{ base: 'md', md: '3xl' }}
+          >
             Join{' '}
-            <Link color='red.600' textDecoration='none' href='https://discord.com/invite/bankless'>
+            <Link
+              color='red.600'
+              textDecoration='none'
+              href='https://discord.com/invite/bankless'
+            >
               BanklessDAO
             </Link>{' '}
             and get in contact with the Podcast Hatchery.
