a8f203420dd50b0c2c1be6e004df6f95676c8319
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-05-28 20:34:51 +0100

    STYLES (join-us) : add mobile styles

--- components/about-us/join-us.tsx
+++ components/about-us/join-us.tsx
@@ -1,5 +1,6 @@
 import { Box, Heading, Text, Stack, Flex } from '@chakra-ui/react'
 import { Container } from '@chakra-ui/react'
+import Underline from 'components/_common/underline'
 
 export default function JoinUsIntroComponent() {
   const pxBase = 0
@@ -10,72 +11,87 @@ export default function JoinUsIntroComponent() {
         as={Box}
         textAlign={'left'}
         spacing={{ base: 4, md: 7 }}
-        py={{ base: 10, md: 18 }}
-        mt='4'
-      >
+        pb={{ base: '4rem', md: '4rem', lg: 0 }}
+        mt={{ lg: '6rem' }}>
         <Heading
-          fontWeight={600}
-          fontSize={{ base: '2xl', md: '3xl' }}
+          alignSelf={'center'}
+          fontFamily='Clear Sans'
+          fontWeight={{ base: 600, md: 800 }}
+          fontSize={{ base: '3xl', md: '4xl' }}
           lineHeight={'110%'}
           color={'white.100'}
-          px={{ base: pxBase, lg: pxLg }}
-        >
+          textTransform={'uppercase'}
+          pb={{ base: '2rem', md: 0 }}>
           Join Us
-          <Container
-            width='200px'
-            filter='blur(5px)'
-            mt={2}
-            mb={2}
-            ml={0}
-            fontSize='16px'
-            lineHeight='8px'
-            background='linear-gradient(
-                        91.88deg,
-                        #ff1013 44.79%,
-                        rgba(140, 29, 207, 0) 101.77%
-                        )'
-          >
-            &nbsp;
-          </Container>
+          <Underline />
         </Heading>
         <Text
-          color={'grey'}
-          px={{ base: pxBase, lg: pxLg }}
-          fontSize={{ base: 'xl', md: '2xl' }}
-        >
-          This open and liberal financial system is just getting started. We’re
+          color='grey'
+          fontSize={{ base: 'xl', lg: '2xl' }}
+          fontFamily='Clear Sans'
+          mt={0}
+          p={0}
+          textAlign={'left'}>
+          {`This open and liberal financial system is just getting started. We're
           building new economies, new ways to work, new thought leadership and
-          scholarship, and new forms of art.
+          scholarship, and new forms of art.`}
         </Text>
 
         <Text
-          color={'white.100'}
-          px={{ base: pxBase, lg: pxLg }}
+          fontFamily='Clear Sans'
           fontSize={{ base: 'xl', md: '2xl' }}
-        >
-          Join us and embark on our mission :
+          fontWeight='bold'
+          color={'white.100'}
+          px={{ base: pxBase, lg: pxLg }}>
+          Join us and embark on our mission:
         </Text>
         <Text
-          color={'grey'}
-          lineHeight={'2rem'}
-          px={{ base: pxBase, lg: pxLg }}
-          marginTop='5px !important'
-          fontSize={{ base: 'xl', md: '2xl' }}
-        >
+          color='grey'
+          fontSize={{ base: 'xl', lg: '2xl' }}
+          fontFamily='Clear Sans'
+          mt={0}
+          p={0}
+          textAlign={'left'}>
           To educate the masses in this new decentralized money system that
-          anyone can use to build wealth and climb the economic ladder. <br />
-          To create new paradigms in how people coordinate, organize, and work.{' '}
-          <br />
-          To be at the forefront of art and culture. <br />
+          anyone can use to build wealth and climb the economic ladder.
+        </Text>
+        <Text
+          color='grey'
+          fontSize={{ base: 'xl', lg: '2xl' }}
+          fontFamily='Clear Sans'
+          mt={0}
+          p={0}
+          textAlign={'left'}>
+          To create new paradigms in how people coordinate, organize, and work.
+        </Text>
+        <Text
+          color='grey'
+          fontSize={{ base: 'xl', lg: '2xl' }}
+          fontFamily='Clear Sans'
+          mt={0}
+          p={0}
+          textAlign={'left'}>
+          To be at the forefront of art and culture.
+        </Text>
+        <Text
+          color='grey'
+          fontSize={{ base: 'xl', lg: '2xl' }}
+          fontFamily='Clear Sans'
+          mt={0}
+          p={0}
+          textAlign={'left'}>
           To provide alpha every day and every hour of the year.
         </Text>
       </Stack>
-      <Flex mt={8} align='center' justify='center'>
-        <Box width={{ base: '100%', md: '60%'}}>
-        <video controls autoPlay loop  muted playsInline>
-          <source src='https://openseauserdata.com/files/6eca99dd059a027eeac7125656ddca9c.mp4#t=0.001' type='video/webm'/>
-          Join Us
-        </video>
+      <Flex align='center' justify='center'>
+        <Box width={{ base: '100%', md: '60%' }}>
+          <video controls autoPlay loop muted playsInline>
+            <source
+              src='https://openseauserdata.com/files/6eca99dd059a027eeac7125656ddca9c.mp4#t=0.001'
+              type='video/webm'
+            />
+            Join Us
+          </video>
         </Box>
       </Flex>
     </>
