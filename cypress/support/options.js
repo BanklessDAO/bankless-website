19915aa4c3517f07e96d3c62fe107a45872843a0
_ <10506480-abc_dev@users.noreply.gitlab.com>
2022-07-20 00:11:11 +0100

    FEAT : adds cypress tests
    Includes breakpoint definitions and pages
    
    Includes setup for snapshot comparrisons
    
    Includes all the snapshots used for visual regression testing

---
+++ cypress/support/options.js
@@ -0,0 +1,33 @@
+export const sizes = [
+  'iphone-5',
+  ['iphone-5', 'landscape'],
+  'samsung-s10',
+  ['samsung-s10', 'landscape'],
+  'iphone-6',
+  ['iphone-6', 'landscape'],
+  'iphone-6+',
+  ['iphone-6+', 'landscape'],
+  'ipad-2',
+  ['ipad-2', 'landscape'],
+  'macbook-11',
+  'macbook-13',
+  'macbook-15',
+  'macbook-16',
+  [1920, 1080],
+  [2048, 1152],
+]
+
+export const pages = [
+  '/',
+  'about-us',
+  'about-us/governance',
+  'about-us/guilds',
+  'about-us/projects',
+  'about-us/community-calls',
+  'about-us/nodes',
+  'work-with-us',
+  'read',
+  'invest',
+  'listen',
+  'new-members',
+]
