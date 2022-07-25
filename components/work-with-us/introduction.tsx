3b46cbc594ee7d0c0aa203859cf7e7ad7b6f133f
_ <10506480-abc_dev@users.noreply.gitlab.com>
2022-07-26 00:52:53 +0100

    STYLES (/work-with-us) : updates the header of work with us

--- components/work-with-us/introduction.tsx
+++ components/work-with-us/introduction.tsx
@@ -4,39 +4,40 @@ import { ChakraNextImage } from 'components/_common/ChakraNextImage'
 export default function IntroductionComponent() {
   return (
     <>
-      <VStack
+      <Box
         as={Box}
         textAlign={'center'}
-        spacing={{ base: 8, md: 14 }}
-        pt={{ base: 0, md: 18 }}
-        pb={{ base: 10, md: 18 }}
-        mb={{base: 0, md:8}}>
+        pt={{ base: 0, md: '4rem' }}
+        pb={{ base: '1rem', md: '6rem' }}>
         <Heading
-          fontWeight={600}
-          fontSize={{ base: '4xl', sm: '3xl', md: '7xl' }}
-          lineHeight={'110%'}
           color={'white.100'}
-          maxW={'94%'}
-          fontFamily='Clear Sans'>
-          {`Web3 products and services \n from crypto-native contributors`}
+          lineHeight={1}
+          fontWeight={700}
+          fontFamily='Clear Sans'
+          fontSize={{ base: '4xl', md: '4xl', lg: '6xl' }}>
+          Web3 products and services
+          <br />
+          from crypto-native contributors
         </Heading>
         <Text
-          color={'white.200'}
-          fontSize={{ base: 'md' }}
-          m={0}
+          color='grey'
+          w={{ base: '100%', xl: '64%' }}
+          mx={'auto'}
+          fontSize={{ base: 'xl', lg: '2xl' }}
+          fontFamily='Clear Sans'
           marginTop={'2rem'}
-          w={{sm: '92%', md: '56%'}}
-          fontFamily='Clear Sans'>
+          textAlign={'center'}>
           BanklessDAO has multiple consulting, media, and development arms
           within its arsenal.
           <br />
-          Commission BanklessDAO’s talent now:
+          <br />
+          Commission BanklessDAO’s talent now.
         </Text>
-      </VStack>
+      </Box>
       <ChakraNextImage
         src='/images/work-with-us/work-with-us-1.jpg'
         alt='Web3 products and services'
-        height={{ base:'224px', lg: '400px' }}
+        height={{ base: '224px', lg: '400px' }}
         width={'full'}
         objectFit='cover'
         borderRadius='104px'
