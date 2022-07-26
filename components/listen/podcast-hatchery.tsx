10542f637d99468ddd2a4a25ce2821a424f130ce
_ <10506480-abc_dev@users.noreply.gitlab.com>
2022-07-26 02:00:38 +0100

    STYLES (/listen) : updates the listen header

--- components/listen/podcast-hatchery.tsx
+++ components/listen/podcast-hatchery.tsx
@@ -1,105 +1,89 @@
 import React from 'react'
-import Image from 'next/image'
-import { Flex, Box, Text, Button, VStack } from '@chakra-ui/react'
-import { HiArrowNarrowRight } from 'react-icons/hi'
+import { Flex, Box, Text, VStack, Heading } from '@chakra-ui/react'
+import { CTAButton } from 'components/_common/CTAButton'
+import { ChakraNextImage } from 'components/_common/ChakraNextImage'
 
-class PodcastHatchery extends React.Component {
-  render() {
-    return (
-      <>
-        <Flex
-          zIndex={'docked'}
-          flexDirection={{ base: 'column-reverse', md: 'row' }}
-          justifyContent='center'
-          alignItems='center'
-        >
-          <VStack
-            maxWidth='600px'
-            mb='4rem'
-            justifyContent='start'
-            alignItems='start'
-            rowGap={6}
-          >
-            <Text
-              fontWeight={700}
-              fontFamily={'clear sans'}
-              fontSize={{ base: '1.4rem', sm: '2rem', lg: '3rem' }}
-            >
-              The Podcast Hatchery
-            </Text>
-            <Text
-              fontFamily={'clear sans'}
-              fontSize={{ base: '12px', md: '1rem', lg: '1.7rem' }}
-              width={{ md: '25rem', lg: '32rem' }}
-            >
-              Incubates audio storytelling projects. The team helps new
-              podcasters find their audience, build their unique storytelling
-              identity, and explore Web3 through interviews with thought-leaders
-              in the space.
-            </Text>
-            <Button
-              onClick={() =>
-                window.open(
-                  'https://open.spotify.com/show/1jLNOoJv77K733rZzL4c6b'
-                )
-              }
-              h={{ base: '4rem', md: '3rem', lg: '4rem' }}
-              w={{ base: '16rem', md: '12rem ', lg: '16rem' }}
-              rounded='md'
-              display={'flex'}
-              alignItems='center'
-            >
-              <Text
-                mt={{ base: '-5px', lg: '-8px' }}
-                mr='5px'
-                fontFamily={'clear sans'}
-                fontSize={{ base: '1.3rem', md: '0.9rem', lg: '1.3rem' }}
-              >
-                Podcast Hatchery
-              </Text>
-              <HiArrowNarrowRight size={'1.8rem'} />
-            </Button>
-            <Text
-              width={{ base: '13rem', md: '20rem', lg: '350px' }}
-              fontSize={{ base: '12px', md: '1rem', lg: '1.2rem' }}
-              fontFamily={'clear sans'}
-              color='grey'
-            >
-              Check out these shows produced and hosted by members of
-              BanklessDAO:
-            </Text>
-          </VStack>
-          <Box
-            mt={{ base: '-1rem', md: '-5rem', lg: '0' }}
-            ml={{ base: '-3rem', md: '0' }}
-            mb={{ base: '5rem', md: '0' }}
-            height={{ base: '16rem', md: '15rem', lg: '30rem' }}
-            width={{ base: '16rem', md: '15rem', lg: '25rem' }}
-          >
-            <Image
-              src='/listen/podcast-hatchery.png'
-              alt='podcast-hatchery'
-              height={500}
-              width={500}
-            />
-          </Box>
-        </Flex>
+const PodcastHatchery = () => (
+  <>
+    <Flex
+      zIndex={'docked'}
+      flexDirection={{ base: 'column', lg: 'row' }}
+      justifyContent='center'
+      alignItems='center'>
+      <VStack
+        mb='4rem'
+        justifyContent='start'
+        alignItems='start'
+        rowGap='2rem'
+        pt={{ base: 0, md: '8rem' }}
+        pb={{ base: 0, md: '4rem' }}>
+        <Heading
+          color={'white.100'}
+          lineHeight={1}
+          fontWeight={700}
+          fontFamily='Clear Sans'
+          fontSize={{ base: '4xl', md: '4xl', lg: '6xl' }}
+          zIndex='1'>
+          The Podcast Hatchery
+        </Heading>
 
-        <Box
-          position='absolute'
-          display={{ base: 'block', md: 'none' }}
-          top={{ base: '5rem' }}
-          left={{ base: '20%' }}
-          bgGradient='linear(to-br, rgba(255, 16, 19, 0.4), rgba(0,0,0,0) 90%)'
-          filter='auto'
-          blur='90px'
-          h={{ base: '25rem' }}
-          w={{ base: '80%', lg: '50rem' }}
-          borderRadius='full'
-        ></Box>
-      </>
-    )
-  }
-}
+        <Text
+          color={'grey'}
+          width={{ md: '25rem', lg: '32rem' }}
+          mx={'auto'}
+          fontSize={{ base: 'xl', lg: '2xl' }}
+          fontFamily='Clear Sans'
+          marginTop={'2rem'}
+          textAlign={'left'}>
+          Incubates audio storytelling projects. The team helps new podcasters
+          find their audience, build their unique storytelling identity, and
+          explore Web3 through interviews with thought-leaders in the space.
+        </Text>
+        <CTAButton
+          url='https://open.spotify.com/show/1jLNOoJv77K733rZzL4c6b'
+          text='Podcast Hatchery'
+        />
+
+        <Text
+          color={'grey'}
+          width={{ md: '25rem', lg: '32rem' }}
+          mx={'auto'}
+          fontSize={{ base: 'xl', lg: '2xl' }}
+          fontFamily='Clear Sans'
+          marginTop={'2rem'}
+          textAlign={'left'}>
+          Check out these shows produced and hosted by members of BanklessDAO:
+        </Text>
+      </VStack>
+      <Box
+        display={{ base: 'none', lg: 'block' }}
+        mt={{ base: '-1rem', md: '-5rem', lg: '-24rem' }}
+        ml={{ base: '0', md: '0', lg: '0' }}
+        mb={{ base: '5rem', md: '0' }}
+        height={{ base: '16rem', md: '15rem', lg: '20rem' }}
+        width={{ base: '16rem', md: '15rem', lg: '20rem' }}>
+        <ChakraNextImage
+          src='/listen/podcast-hatchery.png'
+          alt='podcast-hatchery'
+          height={500}
+          width={500}
+        />
+      </Box>
+    </Flex>
+
+    <Box
+      position='absolute'
+      display={{ base: 'block', md: 'none' }}
+      top={{ base: '5rem' }}
+      left={{ base: '20%' }}
+      bgGradient='linear(to-br, rgba(255, 16, 19, 0.4), rgba(0,0,0,0) 90%)'
+      filter='auto'
+      blur='90px'
+      h={{ base: '25rem' }}
+      w={{ base: '80%', lg: '50rem' }}
+      borderRadius='full'
+    />
+  </>
+)
 
 export default PodcastHatchery
