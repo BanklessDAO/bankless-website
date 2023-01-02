94d61c13858bd7f82c22202e48c0520f343df6e1
MantisClone <david.hunt-mateo@mantisdata.solutions>
2023-01-02 18:33:26 -0500

    feat: add google analytics (#63)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import Script from 'next/script';
 import theme from 'config/theme'
 
 import '../styles/index.css'
@@ -7,11 +8,27 @@ import PageContainer from 'components/_common/page-container'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <PageContainer>
-        <Component {...pageProps} />
-      </PageContainer>
-    </ChakraProvider>
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-PK78Y6EQDB"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-PK78Y6EQDB');
+        `}
+      </Script>
+
+      <ChakraProvider theme={theme}>
+        <PageContainer>
+          <Component {...pageProps} />
+        </PageContainer>
+      </ChakraProvider>
+    </>
   )
 }
 
