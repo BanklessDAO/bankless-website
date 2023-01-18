7b5db0bc2ddacf2c844dc03b552c10c3dfec8748
BOBOtoTHEmoon <114884517+BOBOtoTHEmoon@users.noreply.github.com>
2023-01-18 17:03:54 +0100

    Update guild.tsx

--- components/about-us/sub-pages/guilds/guild.tsx
+++ components/about-us/sub-pages/guilds/guild.tsx
@@ -31,14 +31,14 @@ function Guild() {
           projects. There are 13 guilds within bDAO.
         </Text>
         <VStack mt={8} rowGap={8}>
-          <Section
+<!--           <Section
             title='Analytics Guild'
             text='The Analytics Guild builds data products and uses data analytics to equip
                     bDAO members to achieve our mission with data-driven intelligence.
                     The Analytics Guild also incubated DAO DASH.'
             img='/images/analytic-guild.png'
             alt='analytics-guild'
-          />
+          /> -->
           <Section
             title='Audiovisual Guild'
             text='The AV Guild is the home of BanklessDAO video and audio content.
