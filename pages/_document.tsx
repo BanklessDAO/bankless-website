929e3d84250c74de43aefef002205f2d7c1ffddd
BOBOtoTHEmoon <114884517+BOBOtoTHEmoon@users.noreply.github.com>
2023-01-19 19:02:53 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,7 +16,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <title>Bankess Community</title>
+        <meta name="title" content="Bankess Community" />
+        <meta name="description" content="Add Website description here" />
+        <link rel="shortcut icon" href="favicon.ico" />
+        <link rel="icon" type="image/png" sizes="33x33" href="favicon.png" />
+        <link rel="apple-touch-icon" type="image/png" sizes="33x33" href="favicon.png" />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link
           rel='preconnect'
