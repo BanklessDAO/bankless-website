0bfdbb39cce92d4e58c04e7d19cc44b8db74d6a3
MantisClone <david.hunt-mateo@mantisdata.solutions>
2023-01-20 19:05:18 -0500

    Add BanklessDAO Logo favicon. Fix metadata title and description.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,12 +16,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <title>Bankess Community</title>
-        <meta name="title" content="Bankess Community" />
-        <meta name="description" content="Add Website description here" />
+        <title>BanklessDAO Community</title>
+        <meta name="title" content="BanklessDAO Community" />
+        <meta name="description" content="The BanklessDAO Community website" />
         <link rel="shortcut icon" href="favicon.ico" />
-        <link rel="icon" type="image/png" sizes="33x33" href="favicon.png" />
-        <link rel="apple-touch-icon" type="image/png" sizes="33x33" href="favicon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon.ico" />
+        <link rel="apple-touch-icon" type="image/png" sizes="32x32" href="favicon.ico" />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link
           rel='preconnect'
