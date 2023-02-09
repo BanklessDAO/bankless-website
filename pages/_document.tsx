1432ce3c6f60adc4ee3f1fc91d50c2610fc6e728
Chen Jianjun <chenjj.yz@gmail.com>
2023-02-09 22:32:58 +0800

    use the BanklessDAO Logo 2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,14 +24,14 @@ class MyDocument extends Document {
           <meta property="og:url" content="https://www.bankless.community/" />
           <meta property="og:title" content="BanklessDAO" />
           <meta property="og:description" content="BanklessDAO is a decentralized community with one mission: Help the world gobankless." />
-          <meta property="og:image" content="/icons/bankless-logo.png" />
-          <meta property="og:image:width" content="1000" />
-          <meta property="og:image:height" content="1000" />
+          <meta property="og:image" content="/icons/bankless-logo-2.png" />
+          <meta property="og:image:width" content="480" />
+          <meta property="og:image:height" content="369" />
           <meta property="twitter:url" content="https://www.bankless.community/" />
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:title" content="BanklessDAO" />
           <meta property="twitter:description" content="BanklessDAO is a decentralized community with one mission: Help the world gobankless." />
-          <meta property="twitter:image" content="/icons/bankless-logo.png" />
+          <meta property="twitter:image" content="/icons/bankless-logo-2.png" />
           <link rel="shortcut icon" href="favicon.ico" />
           <link rel="icon" type="image/png" sizes="32x32" href="favicon.ico" />
           <link rel="apple-touch-icon" type="image/png" sizes="32x32" href="favicon.ico" />
