b401f5508075fec178096c2ed4fa41ab0ab1beda
Chen Jianjun <chenjj.yz@gmail.com>
2023-02-18 05:26:54 +0800

    fix(dw-150):Guild page broken on mobile (#117)
    Co-authored-by: MantisClone <david.hunt-mateo@mantisdata.solutions>

--- components/about-us/sub-pages/guilds/guild.tsx
+++ components/about-us/sub-pages/guilds/guild.tsx
@@ -1,21 +1,41 @@
 import React from 'react'
 import Section from './section'
-import { Container, Flex, Text, VStack } from '@chakra-ui/react'
+import { Box, Heading, Stack, Flex, Text, VStack } from '@chakra-ui/react'
 import Underline from 'components/_common/underline'
 
 function Guild() {
   return (
-    <Container maxW={{ base: 'container.sm', md: 'container.xl' }}>
+    <Stack
+        as={Box}
+        textAlign={'center'}
+        spacing={{ base: 2, md: 6 }}
+        py={{ base: 10, md: 18 }}
+        position='relative'
+        zIndex='docked'
+      >
+        <Heading
+          fontWeight={700}
+          fontSize={{
+            base: '2.7rem',
+            sm: '6xl',
+            md: '7xl',
+            lg: '7xl',
+          }}
+          lineHeight={'120%'}
+          color={'white.100'}
+        >
+          Guilds
+          <Box margin='1rem auto' width={{ base: '90%', sm: '100%' }}>
+            <Underline />
+          </Box>
+        </Heading>
+    
       <Flex
         flexDir='column'
         justify='center'
         alignItems='center'
         lineHeight={{ base: '30px', md: '40px' }}
       >
-        <Text fontWeight={700} fontSize='6xl' textAlign='center' maxW={750}>
-          Guilds
-        </Text>
-        <Underline />
         <Text
           textAlign='center'
           mb={10}
@@ -144,7 +164,7 @@ function Guild() {
           />
         </VStack>
       </Flex>
-    </Container>
+    </Stack>
   )
 }
 
