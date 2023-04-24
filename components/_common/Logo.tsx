363ed6c6de6346ffae6cce7448ff9c82d0025693
Yembot31013 <yembot31013@gmail.com>
2023-04-24 16:57:57 +0100

    Add link to homepage on DAO logo

--- components/_common/Logo.tsx
+++ components/_common/Logo.tsx
@@ -15,36 +15,6 @@ const Logo = () => {
           },
         }}
       />
-      {/* <Flex
-        h='100%'
-        direction='column'
-        align='center'
-        justify='center'
-        display={{ base: 'none', md: 'flex' }}
-        sx={{
-          '@media (max-width: 1040px)': {
-            display: 'none',
-          },
-        }}
-      >
-        <Flex
-          p='10px 15px 15px 15px'
-          maxH='150px'
-          bg='#111111'
-          // border='2px solid white'
-          justify='center'
-          align='center'
-          borderRadius='7px'>
-          <Text
-            textDecoration='underline'
-            textDecorationColor='#D02128'
-            textDecorationThickness='5px'
-            fontWeight={800}
-            fontSize='32px'>
-            DAO
-          </Text>
-        </Flex>
-      </Flex> */}
     </Link>
   )
 }
