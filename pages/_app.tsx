f8eb47c743288fe7894419a5ab66b84be20b27b5
MantisClone <david.hunt-mateo@mantisdata.solutions>
2023-05-08 20:04:09 -0400

    Manually record page views because bDAO website is an SPA
      - Based on https://www.makeuseof.com/nextjs-google-analytics/

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,29 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import Script from 'next/script'
+import { useRouter } from 'next/router';
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag"
 import theme from 'config/theme'
 import 'tailwindcss/tailwind.css'
 import '../styles/index.css'
 import PageContainer from 'components/_common/page-container'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+ 
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+ 
+    router.events.on("routeChangeComplete", handleRouteChange);
+ 
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Script
@@ -22,7 +39,6 @@ function MyApp({ Component, pageProps }: AppProps) {
           gtag('config', 'G-PK78Y6EQDB');
         `}
       </Script>
-
       <ChakraProvider theme={theme}>
         <PageContainer>
           <Component {...pageProps} />
