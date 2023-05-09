6b6a1e8d729993d46d317eb9426cac134ddd6421
aorap <orapandrei@gmail.com>
2023-05-09 05:12:18 +0200

    Fix the broken subscribe button on the home page

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -5,7 +5,7 @@ import Section1 from './section1'
 import Section2 from './section2'
 import Section3 from './section3'
 import Section4 from './section4'
-import Section5 from './section5'
+import NewsLetterAboutUs from 'components/_common/newsletter-section'
 import SnapshotSection from './Snapshot/SnapshotSection'
 
 const Home = () => (
@@ -15,7 +15,7 @@ const Home = () => (
     <Section3 />
     {/* <Section4 /> */}
     <SnapshotSection />
-    <Section5 />
+    <NewsLetterAboutUs />
   </VStack>
 )
 
