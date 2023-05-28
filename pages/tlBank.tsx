62a7dce4ccc59071dff9712e6a66523624b8d763
Atolagbe David Tolulope <atolagbedt@gmail.com>
2023-05-29 00:12:49 +0100

    refactor

--- pages/tlBank.tsx
+++ pages/tlBank.tsx
@@ -1,5 +1,7 @@
 import { Network, Alchemy } from 'alchemy-sdk'
 import React, { useState, useEffect, useContext } from 'react'
+import { bankTokenABI, TLBankTokenABI, bankToken, TLBankToken } from '../tlUtils/abi'
+import { MAINNET_RPC_URL, GOERLI_RPC_URL } from '../tlUtils/config'
 import { Select } from '@chakra-ui/react'
 import Logo from '../tlUtils/tlBankLogo'
 import Image from 'next/image'
@@ -60,7 +62,6 @@ import frontierModule from '@web3-onboard/frontier'
 import coinbaseWalletModule from '@web3-onboard/coinbase'
 import walletConnectModule from '@web3-onboard/walletconnect'
 
-const infura = 'process.env.NEXT_PUBLIC_INFURA_API_KEY'
 
 const injected = injectedModule()
 const coinbaseWalletSdk = coinbaseWalletModule()
@@ -76,11 +77,7 @@ const taho = tahoModule() // Previously named Tally Ho wallet
 const trust = trustModule()
 const frontier = frontierModule()
 
-const MAINNET_RPC_URL = `https://mainnet.infura.io/v3/2a4ce5a57b024046b1f2d7ceb2bc0087`
-const GOERLI_RPC_URL = `https://goerli.infura.io/v3/2a4ce5a57b024046b1f2d7ceb2bc0087`
 
-// const MAINNET_RPC_URL = `https://mainnet.infura.io/v3/${infura}`
-// const GOERLI_RPC_URL = `https://goerli.infura.io/v3/${infura}`
 
 const API = '9176eee3-12fa-431c-93c5-27d1f40d4c91'
 
@@ -141,8 +138,6 @@ init({
   },
 })
 
-const bankToken = '0x077154D2931eEC781f8F1a1D0a23Ce6Ef896a2ac' // ERC-20 token
-const TLBankToken = '0x8e6e3b92e4f1818bc7ceee6b7b7228952aa41acb' // NFT token
 
 const settings = {
   apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
@@ -170,7 +165,7 @@ function TlBank() {
   const [tokens, setToken] = useState([])
   const [nftsUri, setNftsUri] = useState([])
   const [data, setData] = useState([])
-  const [selectToken, setSelectToken] = useState<any | null>(null);
+  const [selectToken, setSelectToken] = useState<any | null>(null)
 
   //setAllowance() //setLockedBalance() //()
 
@@ -217,100 +212,6 @@ function TlBank() {
 
     address = wallet?.accounts[0].address
   }
-
-  const bankTokenABI = [
-    {
-      name: 'balanceOf',
-      inputs: [{ internalType: 'address', name: 'account', type: 'address' }],
-      outputs: [{ internalType: 'int256', name: '', type: 'int256' }],
-      stateMutability: 'view',
-      type: 'function',
-    },
-    {
-      name: 'allowance',
-      inputs: [
-        { internalType: 'address', name: 'owner', type: 'address' },
-        { internalType: 'address', name: 'spender', type: 'address' },
-      ],
-      outputs: [{ internalType: 'int256', name: '', type: 'int256' }],
-      stateMutability: 'view',
-      type: 'function',
-    },
-    {
-      name: 'approve',
-      inputs: [
-        { internalType: 'address', name: 'spender', type: 'address' },
-        { internalType: 'uint256', name: 'amount', type: 'uint256' },
-      ],
-      outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
-      stateMutability: 'nonpayable',
-      type: 'function',
-    },
-  ]
-
-  const TLBankTokenABI = [
-    {
-      name: 'createNFT',
-      inputs: [
-        { internalType: 'address', name: 'recipient', type: 'address' },
-        { internalType: 'uint256', name: 'amount', type: 'uint256' },
-        { internalType: 'uint256', name: 'unlockDate', type: 'uint256' },
-      ],
-      outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-      stateMutability: 'nonpayable',
-      type: 'function',
-    },
-    {
-      name: 'lockedBalances',
-      inputs: [{ internalType: 'address', name: 'holder', type: 'address' }],
-      outputs: [
-        { internalType: 'int256', name: 'lockedBalance', type: 'int256' },
-      ],
-      stateMutability: 'view',
-      type: 'function',
-    },
-    {
-      inputs: [{ internalType: 'address', name: 'owner', type: 'address' }],
-      name: 'balanceOf',
-      outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-      stateMutability: 'view',
-      type: 'function',
-    },
-    {
-      inputs: [
-        { internalType: 'address', name: 'owner', type: 'address' },
-        { internalType: 'uint256', name: 'index', type: 'uint256' },
-      ],
-      name: 'tokenOfOwnerByIndex',
-      outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-      stateMutability: 'view',
-      type: 'function',
-    },
-    {
-      inputs: [{ internalType: 'uint256', name: 'tokenId', type: 'uint256' }],
-      name: 'tokenURI',
-      outputs: [{ internalType: 'string', name: '', type: 'string' }],
-      stateMutability: 'view',
-      type: 'function',
-    },
-    {
-      inputs: [
-        { internalType: 'uint256', name: 'tokenId', type: 'uint256' },
-        { internalType: 'uint256', name: 'relockDate', type: 'uint256' },
-      ],
-      name: 'relockNFT',
-      outputs: [],
-      stateMutability: 'nonpayable',
-      type: 'function',
-    },
-    {
-      inputs: [{ internalType: 'uint256', name: 'tokenId', type: 'uint256' }],
-      name: 'redeemNFT',
-      outputs: [],
-      stateMutability: 'nonpayable',
-      type: 'function',
-    },
-  ]
 
   const provider = new ethers.providers.JsonRpcProvider(GOERLI_RPC_URL)
   const BankTokenConDef = new ethers.Contract(bankToken, bankTokenABI, provider)
---
+++ tlUtils/abi.js
@@ -0,0 +1,96 @@
+export const bankTokenABI = [
+  {
+    name: 'balanceOf',
+    inputs: [{ internalType: 'address', name: 'account', type: 'address' }],
+    outputs: [{ internalType: 'int256', name: '', type: 'int256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    name: 'allowance',
+    inputs: [
+      { internalType: 'address', name: 'owner', type: 'address' },
+      { internalType: 'address', name: 'spender', type: 'address' },
+    ],
+    outputs: [{ internalType: 'int256', name: '', type: 'int256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    name: 'approve',
+    inputs: [
+      { internalType: 'address', name: 'spender', type: 'address' },
+      { internalType: 'uint256', name: 'amount', type: 'uint256' },
+    ],
+    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+]
+
+export const TLBankTokenABI = [
+  {
+    name: 'createNFT',
+    inputs: [
+      { internalType: 'address', name: 'recipient', type: 'address' },
+      { internalType: 'uint256', name: 'amount', type: 'uint256' },
+      { internalType: 'uint256', name: 'unlockDate', type: 'uint256' },
+    ],
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    name: 'lockedBalances',
+    inputs: [{ internalType: 'address', name: 'holder', type: 'address' }],
+    outputs: [
+      { internalType: 'int256', name: 'lockedBalance', type: 'int256' },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'address', name: 'owner', type: 'address' }],
+    name: 'balanceOf',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'address', name: 'owner', type: 'address' },
+      { internalType: 'uint256', name: 'index', type: 'uint256' },
+    ],
+    name: 'tokenOfOwnerByIndex',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'uint256', name: 'tokenId', type: 'uint256' }],
+    name: 'tokenURI',
+    outputs: [{ internalType: 'string', name: '', type: 'string' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'uint256', name: 'tokenId', type: 'uint256' },
+      { internalType: 'uint256', name: 'relockDate', type: 'uint256' },
+    ],
+    name: 'relockNFT',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'uint256', name: 'tokenId', type: 'uint256' }],
+    name: 'redeemNFT',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+]
+
+export const bankToken = '0x077154D2931eEC781f8F1a1D0a23Ce6Ef896a2ac' // ERC-20 token
+export const TLBankToken = '0x8e6e3b92e4f1818bc7ceee6b7b7228952aa41acb' // NFT token
