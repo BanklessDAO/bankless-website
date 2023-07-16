a32a6e65a0db7585fd4be8b8413a497de088f950
MantisClone <david.hunt-mateo@mantisdata.solutions>
2023-07-16 00:19:02 -0400

    feat: add tlbank page (#195)
    Co-authored-by: Atolagbe David Tolulope <atolagbedt@gmail.com>
    Co-authored-by: Mark Howard <bark4mark@gmail.com>
    Co-authored-by: Atolagbe David Tolulope <48338938+LordRanchoatos@users.noreply.github.com>
    Co-authored-by: BOBO <idowuoluwanifemi45@gmail.com>

---
+++ tlUtils/abi.js
@@ -0,0 +1,96 @@
+export const bankTokenABI = [
+  {
+    name: 'balanceOf',
+    inputs: [{ internalType: 'address', name: 'account', type: 'address' }],
+    outputs: [{ internalType: 'int256', name: '', type: 'int256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    name: 'allowance',
+    inputs: [
+      { internalType: 'address', name: 'owner', type: 'address' },
+      { internalType: 'address', name: 'spender', type: 'address' },
+    ],
+    outputs: [{ internalType: 'int256', name: '', type: 'int256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    name: 'approve',
+    inputs: [
+      { internalType: 'address', name: 'spender', type: 'address' },
+      { internalType: 'uint256', name: 'amount', type: 'uint256' },
+    ],
+    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+]
+
+export const TLBankTokenABI = [
+  {
+    name: 'createNFT',
+    inputs: [
+      { internalType: 'address', name: 'recipient', type: 'address' },
+      { internalType: 'uint256', name: 'amount', type: 'uint256' },
+      { internalType: 'uint256', name: 'unlockDate', type: 'uint256' },
+    ],
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    name: 'lockedBalances',
+    inputs: [{ internalType: 'address', name: 'holder', type: 'address' }],
+    outputs: [
+      { internalType: 'int256', name: 'lockedBalance', type: 'int256' },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'address', name: 'owner', type: 'address' }],
+    name: 'balanceOf',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'address', name: 'owner', type: 'address' },
+      { internalType: 'uint256', name: 'index', type: 'uint256' },
+    ],
+    name: 'tokenOfOwnerByIndex',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'uint256', name: 'tokenId', type: 'uint256' }],
+    name: 'tokenURI',
+    outputs: [{ internalType: 'string', name: '', type: 'string' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'uint256', name: 'tokenId', type: 'uint256' },
+      { internalType: 'uint256', name: 'relockDate', type: 'uint256' },
+    ],
+    name: 'relockNFT',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'uint256', name: 'tokenId', type: 'uint256' }],
+    name: 'redeemNFT',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+]
+
+export const bankToken = '0x2d94aa3e47d9d5024503ca8491fce9a2fb4da198' // ERC-20 token
+export const TLBankToken = '0xeaeab9f1b25fa00fc01a3fce521b47e88527aa02' // NFT token
---
+++ tlUtils/tlUtil.js
@@ -0,0 +1,109 @@
+export function getCurrentDate(separator = '-') {
+  let newDate = new Date()
+  let date = newDate.getDate()
+  let month = newDate.getMonth() + 1
+  let year = newDate.getFullYear()
+  let hour = newDate.getHours()
+  let min = newDate.getMinutes()
+
+  return `${year}${separator}${
+    month < 10 ? `0${month}` : `${month}`
+  }${separator}${date} ${hour}:${min}`
+}
+
+export function formatEndDate(separator, endate) {
+  let newDate = new Date(endate)
+  let date = newDate.getDate()
+  let month = newDate.getMonth() + 1
+  let year = newDate.getFullYear()
+  let hour = newDate.getHours()
+  let min = newDate.getMinutes()
+
+  return `${year}${separator}${
+    month < 10 ? `0${month}` : `${month}`
+  }${separator}${date} ${hour}:${min}`
+}
+
+export function getUnlockDate(separator, startDate, durationMonths) {
+  // Create a new Date object for the start date
+  const endDate = new Date(startDate)
+
+  // Add the duration in months to the month value
+  endDate.setMonth(endDate.getMonth() + durationMonths)
+
+  // Return the end date
+  // return endDate;
+  let date = endDate.getDate()
+  let month = endDate.getMonth() + 1
+  let year = endDate.getFullYear()
+  let hour = endDate.getHours()
+  let min = endDate.getMinutes()
+
+  return `${year}${separator}${
+    month < 10 ? `0${month}` : `${month}`
+  }${separator}${date} ${hour}:${min}`
+}
+
+export function getUnlockDateRaw(startDate, durationMonths) {
+  // Create a new Date object for the start date
+  const endDate = new Date(startDate)
+
+  // Add the duration in months to the month value
+  endDate.setMonth(endDate.getMonth() + durationMonths)
+
+  return Math.floor(endDate / 1000.0)
+}
+
+export function formatWalletAddress(address) {
+  return `${address.slice(0, 6)}...${address.slice(-4)}`
+}
+
+export function nFormatter(num, digits) {
+  const lookup = [
+    { value: 1, symbol: '' },
+    { value: 1e3, symbol: 'K' },
+    { value: 1e6, symbol: 'M' },
+    { value: 1e9, symbol: 'G' },
+    { value: 1e12, symbol: 'T' },
+    { value: 1e15, symbol: 'P' },
+    { value: 1e18, symbol: 'E' },
+  ]
+  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/
+  var item = lookup
+    .slice()
+    .reverse()
+    .find(function (item) {
+      return num >= item.value
+    })
+  return item
+    ? (num / item.value).toFixed(digits).replace(rx, '$1') + item.symbol
+    : '0'
+}
+
+export function formatDateMm(Ldate, separator = '-') {
+  let newDate = new Date(Ldate * 1000)
+  let date = newDate.getDate()
+  let month = newDate.getMonth() + 1
+  let year = newDate.getFullYear()
+  let hour = newDate.getHours()
+  let min = newDate.getMinutes()
+
+  return `${year}${separator}${
+    month < 10 ? `0${month}` : `${month}`
+  }${separator}${date} ${hour}:${min}`
+}
+
+export function getNewUnlockDateRaw(startDate, durationMonths) {
+  const milliseconds = startDate * 1000;
+  const date = new Date(milliseconds);
+
+  const currentMonth = date.getMonth();
+  date.setMonth(currentMonth + durationMonths);
+
+  // Adjust the day if needed
+  if (date.getDate() < new Date(milliseconds).getDate()) {
+    date.setDate(0);
+  }
+
+  return date.getTime();
+}
