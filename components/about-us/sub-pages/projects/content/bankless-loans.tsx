3db618741c43d57c89013123686419ab1c8df4c8
user <user@users-MacBook-Pro.local>
2023-08-27 22:13:57 +0100

    Fix:I Updated BANKLESS LOANS image on PROJECT’S PAGE

--- components/about-us/sub-pages/projects/content/bankless-loans.tsx
+++ components/about-us/sub-pages/projects/content/bankless-loans.tsx
@@ -30,7 +30,7 @@ export default function BanklessLoansComponent() {
             align={'center'}
             w={'100%'}
             h={'100%'}
-            src='/images/Rectangle 234.png'
+            src='/images/banklessLoan.png'
           />
         </Box>
       </Flex>
