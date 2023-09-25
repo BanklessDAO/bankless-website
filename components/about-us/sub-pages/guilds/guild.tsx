8a3551657ecaa9a27571a63fb96f93d01c9bee3a
Didier Krux <didier@krux.co>
2023-09-25 21:40:19 +0100

    update guilds

--- components/about-us/sub-pages/guilds/guild.tsx
+++ components/about-us/sub-pages/guilds/guild.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import Section from 'components/about-us/section'
 import { Box, Heading, Stack, Flex, Text, VStack } from '@chakra-ui/react'
 import Underline from 'components/_common/underline'
 import styles from 'styles/NavBar.module.css'
+import { GUILDS } from 'constants/data'
+import Section from 'components/_common/Section'
 
 function Guild() {
   return (
@@ -49,89 +50,16 @@ function Guild() {
           projects. There are 13 guilds within bDAO.
         </Text>
         <VStack mt={8} rowGap={8}>
-          <Section
-            title='Project Management Guild'
-            text='A space for BanklessDAO creators to do their best work via project management best practices.
-          We explore how PM best practices can be used within the web3 space and DAOs.'
-            img='/images/project_management_guild.jpg'
-            alt='project management guild'
-          />
-          <Section
-            title='Audiovisual Guild'
-            text='The AV Guild is the home of BanklessDAO video and audio content.
-          From broadcasting the Community Call to building the next version of our audio-recording bot,
-          bDAO contributors rely on the AV Guild for crisp, clear audio solutions for unifying contributors across time zones.'
-            img='/images/eye-analytics.png'
-            alt='av-guild'
-            reverse={true}
-          />
-          <Section
-            title='DAOlationships Guild'
-            text='The DAOlationships Guild facilitates and coordinates relationships between external organizations and
-          the guilds within BanklessDAO.
-          This guild helps us best represent our DAO to anyone outside bDAO.'
-            img='/images/daoLationship.png'
-            alt='daolationship-guild'
-          />
-          <Section
-            title='Design Guild'
-            text='Clear communication of the DAO mission through intelligent next-gen design. We establish powerful symbols, build a
-                  narrative through visual aid, and generate funding through art. We use integrity in our images and
-                  content. As a culture, we reward action and embrace risk.'
-            img='/images/design-guild.png'
-            alt='design-guild'
-            reverse={true}
-          />
-          <Section
-            title='developers guild'
-            text='The Developers Guild builds tooling, provides developer education, and helps developers do their best work in Web3.
-          They also incubated the DEGEN,
-          Gov-Bot, Mobile App and Bounty Board projects.'
-            img='/images/DevGuild.png'
-            alt='dev-guild'
-          />
-
-          <Section
-            title='legal guild'
-            text='The Legal Guild authored the BANK legal assessment and white paper.
-           It also provides regular updates on the legal developments in crypto and DAO governance.
-           They also incubated the Decentralized Law newsletter and legal podcast project.'
-            img='/images/legal-guild.png'
-            alt='legal-guild'
-            reverse={true}
-          />
-
-          <Section
-            title='Research guild'
-            text='The Research Guild operates as a support node for conducting, guiding, and funding research that aligns with BanklessDAO’s mission.'
-            img='/images/research-guild.png'
-            alt='research-guild'
-          />
-
-          <Section
-            title='Translators guild'
-            text='The Translators Guild spreads the Bankless mission across the world by translating our media into dozens of languages,
-           then publishing them through the International Media Nodes.'
-            img='/images/translators-guild.png'
-            alt='translator-guild'
-            reverse={true}
-          />
-          <Section
-            title='Writers guild'
-            text='The Writers Guild is the umbrella for all writing projects coming out of the DAO. They incubated the Newsletter project,
-           the Editing/Publishing Arm, and the Client Services project. '
-            img='/images/writers-guild.png'
-            alt='writers-guild'
-          />
-          <Section
-            title='Analytics Guild (INACTIVE)'
-            text='The Analytics Guild builds data products and uses data analytics to equip
-                    bDAO members to achieve our mission with data-driven intelligence.
-                    The Analytics Guild also incubated DAO DASH.'
-            img='/images/analytic-guild.png'
-            alt='analytics-guild'
-            reverse={true}
-          />
+          {GUILDS.map((guild, index) => (
+            <Section
+              title={guild.name}
+              text={guild.description}
+              img={guild.image}
+              alt={guild.name}
+              reverse={index % 2 === 1}
+              key={`guild-${index}`}
+            />
+          ))}
         </VStack>
       </Flex>
     </Stack>
