14de09510e2d38f550f7459ae576b1cefa78d1ae
Didier Krux <didier@krux.co>
2023-09-25 21:40:30 +0100

    update projects

--- components/about-us/sub-pages/projects/content.tsx
+++ components/about-us/sub-pages/projects/content.tsx
@@ -1,51 +1,20 @@
 import { Box } from '@chakra-ui/react'
-import BanklessAcademyComponent from './content/bankless-academy'
-import ProjectBanklessConsultingComponent from './content/bankless-consulting'
-import BanklessLoansComponent from './content/bankless-loans'
-import ProjectBountyBoardComponent from './content/bounty-board'
-import CryptoSapiensComponent from './content/crypto-sapiens'
-import DaoversityComponent from './content/daoversity'
-import DecentralizeLawNewsletterComponent from './content/decentralizes-law-newsletter'
-import EditArmProjectComponent from './content/edit-arm'
-import FightClubComponent from './content/fight-club'
-import FirstQuestComponent from './content/first-quest'
-import InternationalMediaNodesComponent from './content/international-media-nodes'
-import NewsletterProjectComponent from './content/newsletter-project'
-import PodcastHatcheryComponent from './content/podcast-hatchery'
-import WebsiteProjectComponent from './content/website-project'
-import InfoSecTeamComponent from './content/infosec-team'
-import BanklessCardComponent from './content/bankless-card'
-import GovernatorComponent from './content/governator'
-import TheRugComponent from './content/the-rug'
-import MakingBankComponent from './content/making-bank'
-import BanklessLabsComponent from './content/bankless-labs'
-import BanklessAfricaComponent from './content/bankless-africa'
-import DaostewardsComponent from './content/daostewards'
+import Section from 'components/_common/Section'
+import { PROJECTS } from 'constants/data'
+
 export default function ProjectsContentComponent() {
   return (
     <Box display={'flex'} flexDir='column' alignItems={'center'}>
-      <BanklessCardComponent />
-      <ProjectBanklessConsultingComponent />
-      <ProjectBountyBoardComponent />
-      <NewsletterProjectComponent />
-      <EditArmProjectComponent />
-      <PodcastHatcheryComponent />
-      <DaoversityComponent />
-      <FightClubComponent />
-      <BanklessLoansComponent />
-      <BanklessAcademyComponent />
-      <CryptoSapiensComponent />
-      <FirstQuestComponent />
-      <InternationalMediaNodesComponent />
-      <DecentralizeLawNewsletterComponent />
-      <InfoSecTeamComponent />
-      <WebsiteProjectComponent />
-      <GovernatorComponent />
-      <TheRugComponent />
-      <MakingBankComponent />
-      <BanklessLabsComponent />
-      <BanklessAfricaComponent />
-      <DaostewardsComponent />
+      {PROJECTS.map((guild, index) => (
+        <Section
+          title={guild.name}
+          text={guild.description}
+          img={guild.image}
+          alt={guild.name}
+          reverse={index % 2 === 1}
+          key={`guild-${index}`}
+        />
+      ))}
     </Box>
   )
 }
