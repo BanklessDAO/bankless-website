8570ec27f74b78f0134dbc7e3f755a8675cafcfd
Didier Krux <didier@krux.co>
2023-09-25 20:55:51 +0100

    project & guild data

---
+++ constants/data.ts
@@ -0,0 +1,40 @@
+import { ProjectType } from 'entities/project'
+
+export const GUILDS: ProjectType[] = [
+  {
+    "name": "Project Management Guild",
+    "type": "guild",
+    "description": "A space for BanklessDAO creators to do their best work via project management best practices. We explore how PM best practices can be used within the web3 space and DAOs.",
+    "image": "/images/guild/project-management-guild-2fcc8bd2.jpg"
+  },
+  {
+    "name": "Audiovidual Guild",
+    "type": "guild",
+    "description": "The AV Guild is the home of BanklessDAO video and audio content. From broadcasting the Community Call to building the next version of our audio-recording bot, bDAO contributors rely on the AV Guild for crisp, clear audio solutions for unifying contributors across time zones.",
+    "image": "/images/guild/audiovidual-guild-f901620b.png"
+  }
+]
+
+export const PROJECTS: ProjectType[] = [
+  {
+    "name": "Bankless Card",
+    "type": "project",
+    "description": "Bankless Card aims to provide bootstrap funding to every community through card interchange revenues. DAO members can use Bankless Card for their everyday purchases to earn cashback in the form of DAO social tokens. New contributors can also learn core web3 concepts through in-app integrations (i.e. Snapshot, PoolTogether, and more).",
+    "image": "/images/project/bankless-card-5fa77bc9.png",
+    "link": "https://banklesscard.xyz/"
+  },
+  {
+    "name": "Bankless Consulting",
+    "type": "project",
+    "description": "Bankless Consulting is the consulting services arm of BanklessDAO. It connects over 15,000+ talented Web3 natives to professional services in marketing, tech, legal, finance, and management. Bankless Consulting helps decentralized, Web3-native, and traditional businesses leverage the power of Web3.",
+    "image": "/images/project/bankless-consulting-44709f18.png",
+    "link": "https://www.banklessconsulting.com/"
+  },
+  {
+    "name": "Bounty Board",
+    "type": "project",
+    "description": "The Bounty Board creates an efficient mechanism of coordination between organizers and contributors. It provides a way to quickly access Web3 talent within the DAO for bountied work.",
+    "image": "/images/project/bounty-board-7aee2b5b.png",
+    "link": "https://bankless.notion.site/Bounty-Board-Project-Page-318dc164cc5640cca17e0fb5f484fd90"
+  }
+]
