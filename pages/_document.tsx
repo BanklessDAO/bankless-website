bc93143e5cd2ca3910a2fca678621d1da0f73e48
MantisClone <david.hunt-mateo@mantisdata.solutions>
2023-10-03 16:14:23 -0400

    Remove line causing error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,7 +29,10 @@ class MyDocument extends Document {
             property='og:description'
             content='BanklessDAO is a decentralized community with one mission: Help the world gobankless.'
           />
-          <meta property='og:image' content='/bankless-website/icons/bankless-logo-2.png' />
+          <meta
+            property='og:image'
+            content='/bankless-website/icons/bankless-logo-2.png'
+          />
           <meta property='og:image:width' content='480' />
           <meta property='og:image:height' content='369' />
           <meta
@@ -42,9 +45,17 @@ class MyDocument extends Document {
             property='twitter:description'
             content='BanklessDAO is a decentralized community with one mission: Help the world gobankless.'
           />
-          <meta property='twitter:image' content='/bankless-website/icons/bankless-logo-2.png' />
+          <meta
+            property='twitter:image'
+            content='/bankless-website/icons/bankless-logo-2.png'
+          />
           <link rel='shortcut icon' href='/bankless-website/favicon.ico' />
-          <link rel='icon' type='image/png' sizes='32x32' href='/bankless-website/favicon.ico' />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/bankless-website/favicon.ico'
+          />
           <link
             rel='apple-touch-icon'
             type='image/png'
@@ -53,11 +64,7 @@ class MyDocument extends Document {
           />
         </Head>
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link
-          rel='preconnect'
-          href='https://fonts.gstatic.com'
-          crossOrigin='true'
-        />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
         <body>
           <Main />
           <NextScript />
