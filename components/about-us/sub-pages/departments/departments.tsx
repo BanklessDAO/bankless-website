68e7d2a5679f586b3089f784d7da39c492230c92
Didier Krux <didier@krux.co>
2023-10-04 19:14:26 +0100

    update departments

--- components/about-us/sub-pages/departments/departments.tsx
+++ components/about-us/sub-pages/departments/departments.tsx
@@ -3,6 +3,7 @@ import Section from 'components/about-us/section'
 import { Box, Heading, Stack, Flex, Text, VStack } from '@chakra-ui/react'
 import Underline from 'components/_common/underline'
 import styles from 'styles/NavBar.module.css'
+import { DEPARTMENTS } from 'constants/data'
 
 function Departments() {
   return (
@@ -13,8 +14,7 @@ function Departments() {
       py={{ base: 10, md: 18 }}
       position='relative'
       zIndex='docked'
-      className={`${styles['header-text']}`}
-    >
+      className={`${styles['header-text']}`}>
       <Heading
         fontWeight={700}
         fontSize={{
@@ -24,8 +24,7 @@ function Departments() {
           lg: '7xl',
         }}
         lineHeight={'120%'}
-        color={'white.100'}
-      >
+        color={'white.100'}>
         Departments
         <Box margin='1rem auto' width={{ base: '90%', sm: '100%' }}>
           <Underline />
@@ -36,8 +35,7 @@ function Departments() {
         flexDir='column'
         justify='center'
         alignItems='center'
-        lineHeight={{ base: '30px', md: '40px' }}
-      >
+        lineHeight={{ base: '30px', md: '40px' }}>
         <Text
           textAlign='center'
           mb={10}
@@ -45,39 +43,22 @@ function Departments() {
           maxW={826}
           lineHeight={{ base: '40px', md: '46px' }}
           fontWeight='400px'
-          zIndex={1}
-        >
+          zIndex={1}>
           Departments are operation-critical units of BanklessDAO. The
           qualification test for a department is that if it does not exist, the
           DAO cannot function properly or may cease to exist!
         </Text>
         <VStack mt={8} rowGap={8}>
-          <Section
-            title='Marketing Department'
-            text='The Marketing Department is dedicated to amplifying the values of BanklessDAO, accomplishing this by delivering creative Web3 marketing solutions that effectively promote BanklessDAO initiatives, partners, and clients to their target audience with precision. Despite not yet adapting our own values, we remain aligned with those of the DAO.'
-            img='/images/marketing-guild.png'
-            alt='Marketing Department'
-          />
-          <Section
-            title='Operations Department'
-            text='The operations department is responsible for overseeing daily activities within the DAO, ensuring alignment with the general mission and vision, and facilitating core operations, budget management, and the seamless functioning of the community, all while upholding the values of inclusivity and cooperation.'
-            img='/images/operation-guild.png'
-            alt='Operations Department'
-            reverse={true}
-          />
-          <Section
-            title='Education Department'
-            text='The Education Department serves as a cornerstone of empowerment within the DAO, taking an internal focus in its efforts to onboard new members, impart essential DAO tool knowledge, and offer informational sessions, all with the overarching goal of helping individuals become more effective participants in the BanklessDAO community.'
-            img='/images/edu-guild.png'
-            alt='Education Department'
-          />
-          <Section
-            title='Treasury Department'
-            text='The Treasury Department`s mission is to safeguard and improve the DAO`s financial infrastructure, driven by values such as transparency, integrity, responsibility, and accountability. Under Treasury Department Accounting, it oversees financial matters, reports, assets, budgets, and meticulous records to ensure responsible financial management.'
-            img='/images/treasury-guild.png'
-            alt='Treasury Department'
-            reverse={true}
-          />
+          {DEPARTMENTS.map((department, index) => (
+            <Section
+              title={department.name}
+              text={department.description}
+              img={department.image}
+              alt={department.name}
+              reverse={index % 2 === 1}
+              key={`department-${index}`}
+            />
+          ))}
         </VStack>
       </Flex>
     </Stack>
