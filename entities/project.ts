b68c740ffbc4eebd3f170126f41af399c85ea6f5
Didier Krux <didier@krux.co>
2023-10-14 12:37:05 +0100

    fix category type

--- entities/project.ts
+++ entities/project.ts
@@ -1,7 +1,7 @@
 export interface ProjectType {
   name: string,
   page: string,
-  category?: 'consulting-and-education' | 'sponsorships-and-marketing' | 'creative-services', 'software-and-dao-tooling',
+  category?: 'consulting-and-education' | 'sponsorships-and-marketing' | 'creative-services' | 'software-and-dao-tooling',
   description: string,
   image: string,
   link?: string,
