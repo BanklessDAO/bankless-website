2e875b478dbfd40968464edcf0762b199761b6c4
Didier Krux <didier@krux.co>
2023-10-28 19:40:53 +0100

    improve News display

--- components/home/news.tsx
+++ components/home/news.tsx
@@ -1,5 +1,17 @@
 import React, { useEffect, useState } from 'react'
-import { Box, Link, ListItem, UnorderedList } from '@chakra-ui/react'
+import { Image, Box, Heading, Link } from '@chakra-ui/react'
+import styled from '@emotion/styled'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import {
+  Mousewheel,
+  Keyboard,
+  Autoplay,
+  Pagination,
+  Navigation,
+} from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
 
 export interface FeedType {
   title: string
@@ -10,12 +22,67 @@ export interface FeedType {
 }
 
 export interface NewsType {
+  announcement: string
   feed: FeedType[]
   featured: FeedType[]
 }
 
+const Announcement = styled(Box)`
+  a {
+    text-decoration: underline;
+  }
+`
+
+const FeaturedNews = styled(Box)`
+  * {
+    --swiper-theme-color: red;
+  }
+  .swiper {
+    width: 100%;
+    height: 100%;
+  }
+
+  .swiper-slide {
+    text-align: center;
+
+    /* Center slide text vertically */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .swiper-slide img {
+    display: block;
+    width: 100%;
+    object-fit: cover;
+  }
+  .swiper-pagination {
+    /* bottom: -20px; */
+  }
+`
+
+const formatDateDescription = targetDate => {
+  const currentDate = new Date()
+  const timeDifference =
+    currentDate.getTime() - Date.parse(targetDate).valueOf()
+  const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
+
+  if (daysDifference === 0) {
+    return 'today'
+  } else if (daysDifference === 1) {
+    return 'yesterday'
+  } else if (daysDifference <= 7) {
+    return `${daysDifference} days ago`
+  } else if (daysDifference <= 14) {
+    return 'last week'
+  } else {
+    const weeksDifference = Math.floor(daysDifference / 7)
+    return `${weeksDifference} weeks ago`
+  }
+}
+
 const News = (): React.ReactElement => {
   const [latestNews, setLatestNews] = useState<NewsType>({
+    announcement: '',
     feed: [],
     featured: [],
   })
@@ -34,32 +101,60 @@ const News = (): React.ReactElement => {
   }, [])
 
   return (
-    <Box zIndex={1}>
-      <Box>Announcement</Box>
-      <Box>
-        <Box>
-          Featured news
-          <UnorderedList>
-            {latestNews.featured.map((news, index) => (
-              <ListItem key={`news-${index}`}>
-                <Link href={news.link} isExternal>
-                  {news.website} - {news.title}
-                </Link>
-              </ListItem>
-            ))}
-          </UnorderedList>
+    <Box zIndex={1} w='100%'>
+      <Announcement>
+        <Heading
+          dangerouslySetInnerHTML={{
+            __html: latestNews.announcement,
+          }}></Heading>
+      </Announcement>
+      <Box display={{ base: 'block', xl: 'flex' }}>
+        <Box display='block' width='70%'>
+          <FeaturedNews>
+            <Swiper
+              spaceBetween={30}
+              centeredSlides={true}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: true,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              navigation={true}
+              modules={[Mousewheel, Keyboard, Autoplay, Pagination, Navigation]}
+              className='mySwiper'>
+              {latestNews.featured.map((news, index) => (
+                <SwiperSlide key={`news-${index}`}>
+                  <Image
+                    src={news.image}
+                    w='100%'
+                    aspectRatio='1.9'
+                    alt={`${news.website} - ${news.title}`}
+                  />
+                  <Box position='absolute'>
+                    <Link href={news.link} isExternal>
+                      {news.website} - {news.title}
+                    </Link>
+                  </Box>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </FeaturedNews>
         </Box>
-        <Box>
-          Latest news
-          <UnorderedList>
+        <Box m={4}>
+          <Heading>Latest news</Heading>
+          <Box>
             {latestNews.feed.map((news, index) => (
-              <ListItem key={`news-${index}`}>
+              <Box key={`news-${index}`} mt={2}>
                 <Link href={news.link} isExternal>
-                  {news.website} - {news.title}
+                  {news.title}
                 </Link>
-              </ListItem>
+                <br />
+                {news.website} • {formatDateDescription(news.pubDate)}
+              </Box>
             ))}
-          </UnorderedList>
+          </Box>
         </Box>
       </Box>
     </Box>
