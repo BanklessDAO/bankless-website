eaa47775436c030dffccbb774e5fbea36e9ff7e4
Didier Krux <didier@krux.co>
2023-11-11 17:50:01 +0000

    Create basic card component

---
+++ components/_common/Card.tsx
@@ -0,0 +1,15 @@
+import { Box } from '@chakra-ui/react'
+import styled from '@emotion/styled'
+
+const Card = styled(Box)`
+  padding: 16px 24px;
+  border-radius: 12px;
+  border: 1px solid #333a40;
+  background: linear-gradient(
+    180deg,
+    rgba(28, 28, 29, 0.5) 0,
+    rgba(51, 58, 64, 0.5) 100%
+  );
+`
+
+export default Card
