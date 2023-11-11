1824764225872f5f4aceb0035f863128b7eb9a19
Didier Krux <didier@krux.co>
2023-11-11 17:50:26 +0000

    improve news style

--- components/home/news.tsx
+++ components/home/news.tsx
@@ -13,6 +13,8 @@ import 'swiper/css'
 import 'swiper/css/pagination'
 import 'swiper/css/navigation'
 
+import Card from 'components/_common/Card'
+
 export interface FeedType {
   title: string
   link: string
@@ -27,7 +29,7 @@ export interface NewsType {
   featured: FeedType[]
 }
 
-const Announcement = styled(Box)`
+const Announcement = styled(Card)`
   a {
     text-decoration: underline;
   }
@@ -36,6 +38,10 @@ const Announcement = styled(Box)`
 const FeaturedNews = styled(Box)`
   * {
     --swiper-theme-color: red;
+  }
+  .mySwiper {
+    border-radius: 12px;
+    overflow: hidden;
   }
   .swiper {
     width: 100%;
@@ -57,6 +63,15 @@ const FeaturedNews = styled(Box)`
   }
   .swiper-pagination {
     /* bottom: -20px; */
+  }
+  .swiper-pagination-bullet {
+    width: 11px;
+    height: 11px;
+    background: white;
+    opacity: 0.8;
+  }
+  .swiper-pagination-bullet-active {
+    background: red !important;
   }
 `
 
@@ -93,7 +108,7 @@ const News = (): React.ReactElement => {
         .then(response => response.json())
         .then(response => {
           console.log(response)
-          setLatestNews(response)
+          setLatestNews(response.data)
         })
     } catch (error) {
       console.error(error)
@@ -102,14 +117,18 @@ const News = (): React.ReactElement => {
 
   return (
     <Box zIndex={1} w='100%'>
-      <Announcement>
+      <Announcement mb='6'>
         <Heading
           dangerouslySetInnerHTML={{
             __html: latestNews.announcement,
           }}></Heading>
       </Announcement>
       <Box display={{ base: 'block', xl: 'flex' }}>
-        <Box display='block' width='70%'>
+        <Box
+          display='block'
+          width={{ base: '100%', xl: '70%' }}
+          mr={{ base: '0', xl: '4' }}
+          mb={{ base: '4', xl: '0' }}>
           <FeaturedNews>
             <Swiper
               spaceBetween={30}
@@ -121,40 +140,60 @@ const News = (): React.ReactElement => {
               pagination={{
                 clickable: true,
               }}
-              navigation={true}
+              navigation={false}
               modules={[Mousewheel, Keyboard, Autoplay, Pagination, Navigation]}
               className='mySwiper'>
               {latestNews.featured.map((news, index) => (
                 <SwiperSlide key={`news-${index}`}>
-                  <Image
-                    src={news.image}
-                    w='100%'
-                    aspectRatio='1.9'
-                    alt={`${news.website} - ${news.title}`}
-                  />
-                  <Box position='absolute'>
-                    <Link href={news.link} isExternal>
-                      {news.website} - {news.title}
-                    </Link>
-                  </Box>
+                  <Link href={news.link} isExternal>
+                    <Image
+                      src={news.image}
+                      w='100%'
+                      aspectRatio='1.9'
+                      alt={`${news.website} - ${news.title}`}
+                    />
+                    <Box
+                      position='absolute'
+                      backgroundColor='black'
+                      opacity='0.4'
+                      top='0'
+                      left='0'
+                      w='100%'
+                      height='100%'></Box>
+                    <Box
+                      position='absolute'
+                      left='12px'
+                      bottom='12px'
+                      textAlign='left'
+                      color='white'>
+                      <Heading as='h2' size='lg'>
+                        {news.title}
+                      </Heading>
+                      {news.website}
+                    </Box>
+                  </Link>
                 </SwiperSlide>
               ))}
             </Swiper>
           </FeaturedNews>
         </Box>
-        <Box m={4}>
-          <Heading>Latest news</Heading>
-          <Box>
+        <Box>
+          <Card>
+            <Heading as='h2' size='lg'>
+              Latest News
+            </Heading>
             {latestNews.feed.map((news, index) => (
               <Box key={`news-${index}`} mt={2}>
-                <Link href={news.link} isExternal>
+                <Link href={news.link} isExternal color='white'>
                   {news.title}
                 </Link>
                 <br />
-                {news.website} • {formatDateDescription(news.pubDate)}
+                <Box color='whiteAlpha.500'>
+                  {news.website} • {formatDateDescription(news.pubDate)}
+                </Box>
               </Box>
             ))}
-          </Box>
+          </Card>
         </Box>
       </Box>
     </Box>
